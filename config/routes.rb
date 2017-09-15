0193d69ff042bf15e8e223cf39833204fbbffc66
saravanakumar.k@zohocorp.com <saravanakumar.k@zohocorp.com>
2017-09-15 08:50:09 +0530

    Signin

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ root 'static_pages#home'
   get  '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
 
-  resource 'users'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
