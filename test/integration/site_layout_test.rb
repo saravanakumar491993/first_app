263e19fd5080d6a2df6123b8eb5471a9ef28325e
saravanakumar.k@zohocorp.com <saravanakumar.k@zohocorp.com>
2017-09-15 07:25:15 +0530

    Modelling users

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -11,7 +11,12 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
-     get contact_path
+	  assert_select "a[href=?]", signup_path
+
+    get contact_path
     assert_select "title", full_title("Contact")
-  end
+	
+	  get signup_path
+	  assert_select "title", full_title("Signup")
+end
 end
